e10086597c281a6eb48d580e73d0fdbd6465a4fb
=nqi <=nqi@pch.com>
2017-09-27 14:59:52 -0400

    format sort layout
    update helpers function location
    reformat layout
    rename array helper class

--- public/Sort.php
+++ public/Sort.php
@@ -2,23 +2,27 @@
 
 require __DIR__ . './../vendor/autoload.php';
 
-use Algorithm\Helpers\SortTestHelper AS Helper;
 use Algorithm\Sort\BasicSort;
 use Algorithm\Sort\AdvancedSort;
 use Algorithm\Configuration\ArrayConfig;
+use Algorithm\Helpers\SortHelper as Helper;
 
 /** Prepare Area */
-$config = [ 10, 0, 1000 ]; 
-$helper = new Helper( ... $config);
+$config = [ 20, 0, 1000 ];
+$helper = new Helper(... $config);
+$helper->setNumberPerline(15);
 
 /**  Sort  **/
-// OPTIONS FOR ARRAY SORTING: 
+// OPTIONS FOR ARRAY SORTING:
 // SELECTION, INSERTION, UPDATED_INSERTION
 // MERGE, QUICK
-display([ new BasicSort(ArrayConfig::INSERTION, $helper), new BasicSort(ArrayConfig::SELECTION, $helper),]);
+$toCompare =[
+	// BASIC SORT
+    new BasicSort(ArrayConfig::INSERTION, $helper),
+    new BasicSort(ArrayConfig::SELECTION, $helper),
+    new BasicSort(ArrayConfig::UPDATED_INSERTION, $helper),
 
+	// ADVANCED SORT
 
-
-
-
-
+] ;
+display($toCompare);
